9ff76a03598a85b5868421d6a49a0db012829b61
JoranHonig <JoranHonig@users.noreply.github.com>
2024-04-08 14:13:02 +0200

    :sparkles: add kyc filter

--- bountyhunter/cli/list.py
+++ bountyhunter/cli/list.py
@@ -7,8 +7,13 @@ from bountyhunter.immunefi.dataprovider import ImmunefiDataProvider
 
 @command(help="List bug bounty programmes")
 @option("--limit", default=10, help="Limit the number of programmes to display")
-@option("--show-high-reward", is_flag=True, help="Display the reward for high reward programmes")
-def list(limit, show_high_reward):
+@option(
+    "--show-high-reward",
+    is_flag=True,
+    help="Display the reward for high reward programmes",
+)
+@option("--only-kyc", is_flag=True, help="Only show programs that require KYC")
+def list(limit, show_high_reward, only_kyc):
     immunefi_data_provider = ImmunefiDataProvider()
     programs = immunefi_data_provider.get_immunefi_programs()
 
@@ -20,24 +25,36 @@ def list(limit, show_high_reward):
 
     table.add_column("Project", justify="left")
     table.add_column("Max Bounty", justify="right")
-    table.add_column('KYC Required', justify='center')
+    table.add_column("KYC Required", justify="center")
     # table.add_column('Primacy', justify='center')
 
-    programs = programs[:limit]
+    count = 0
 
     for program in programs:
-        program_info = immunefi_data_provider.get_program_info(program.get('id'))
+        if count >= limit:
+            break
 
-        kycrequired = (program_info.get('bounty', {}).get('kyc', False))
+        program_info = immunefi_data_provider.get_program_info(program.get("id"))
+
+        kycrequired = program_info.get("bounty", {}).get("kyc", False)
+
+        if only_kyc and not kycrequired:
+            continue
+
         # turn into check mark icon
         kycrequired = "\uf00c" if kycrequired else "\uf467"
 
-        primacy_of_impact = any(impact.get('type', "").startswith("primacy") for impact in program_info.get('bounty', {}).get('impacts', []))
+        primacy_of_impact = any(
+            impact.get("type", "").startswith("primacy")
+            for impact in program_info.get("bounty", {}).get("impacts", [])
+        )
         primacy_of_impact = "\uf00c" if primacy_of_impact else "\uf467"
 
-        maximum_reward = program.get('maximum_reward')
+        maximum_reward = program.get("maximum_reward")
         human_readable_max_reward = f"[green]${maximum_reward:,}[/green]"
 
-        table.add_row(program.get('project'), human_readable_max_reward, kycrequired)
+        table.add_row(program.get("project"), human_readable_max_reward, kycrequired)
+        count += 1
 
     console.print(table)
+
